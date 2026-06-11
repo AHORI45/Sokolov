0b72875b141617818e4ae971f63b33d308bf6c2b
AHORI45 <sokolnikita07@gmail.com>
2026-06-12 01:39:41 +0300

    Add files via upload

---
+++ practice/sklv prc/src/exceptions/DBException.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . 'src/exceptions/DbExceptions.php';
+
+spl_autoload_register(function($class) {
+    $classPath = str_replace('\\', '/', $class);
+    $file = dirname(__DIR__) . '/' . $classPath . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
